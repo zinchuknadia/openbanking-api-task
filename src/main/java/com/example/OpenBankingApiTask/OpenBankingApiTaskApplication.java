1a684f093f3cc5ccfcfaa4ee6479e25c13e65771
zinchuknadia <nadiia.zinchuk.oi.2023@lpnu.ua>
2026-03-16 12:32:50 +0200

    first commit

---
+++ src/main/java/com/example/OpenBankingApiTask/OpenBankingApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.OpenBankingApiTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBankingApiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenBankingApiTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OpenBankingApiTask/OpenBankingApiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OpenBankingApiTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBankingApiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
